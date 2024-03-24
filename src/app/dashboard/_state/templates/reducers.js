13f4e7850a5c611807968c15a6964b88f9b09d4a
vidothedesigner <48659615+vidothedesigner@users.noreply.github.com>
2024-03-24 22:08:22 +0100

    posledno

--- src/app/dashboard/_state/templates/reducers.js
+++ src/app/dashboard/_state/templates/reducers.js
@@ -12,19 +12,14 @@ import {
 export const templatesReducer = (draft, action) => {
 	switch (action.type) {
 		case SET_ANALISYS_TYPE: {
-			console.log(action, 'SET_ANALISYS_TYPE RAN');
 			draft.analisysType = action.payload;
 			break;
 		}
 		case SET_PRODUCT: {
-			console.log(action, 'SET_PRODUCT RAN');
-
 			draft.product = action.payload;
 			break;
 		}
 		case SET_INPUT: {
-			// console.log(action, 'SET_INPUT RAN');
-
 			const index = draft.inputData.findIndex(
 				(item) => JSON.stringify(item[0]) === JSON.stringify(action.payload[0])
 			);
@@ -37,26 +32,17 @@ export const templatesReducer = (draft, action) => {
 			break;
 		}
 		case ADD_TEMPLATE_ITEM: {
-			// console.log(action, 'ADD_TEMPLATE_ITEM RAN');
-
 			draft.templateData.push(draft.inputData);
 			break;
 		}
 
 		case DELETE_TEMPLATE_ITEM: {
-			// console.log(action, 'DELETE_TEMPLATE_ITEM RAN');
-
-			// let getDeletedItem = draft.templateData[action.payload];
-			// console.log(getDeletedItem, 'in REDUCER');
 			draft.templateData.splice(action.payload.index, 1);
 			draft.deleted.push(action.payload.row);
 			break;
 		}
 
 		case SET_STATE: {
-			console.log(action, 'SET_STATE RAN');
-
-			// console.log(action, 'in reducer');
 			draft.id = action.payload._id;
 			draft.analisysType = action.payload.analisysType;
 			draft.product = action.payload.product;
@@ -67,14 +53,10 @@ export const templatesReducer = (draft, action) => {
 			break;
 		}
 		case SHOULD_UPDATE: {
-			// console.log(action, 'SHOULD_UPDATE RAN');
-
 			draft.shouldUpdate = action.payload;
 			break;
 		}
 		case RESET: {
-			console.log(action, 'RESET RAN');
-
 			draft.id = '';
 			draft.analisysType = '';
 			draft.product = '';
