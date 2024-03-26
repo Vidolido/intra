225b10987e9d25bd5ededc0422f79cdde6fa83a8
vidothedesigner <48659615+vidothedesigner@users.noreply.github.com>
2024-03-26 22:44:43 +0100

    Update imports and refactor code in various files
    In conn.js, update the import of mongoose and remove unused variables.
    In next.config.mjs, update the import of NextConfig.
    In [setting]/page.jsx, update imports and refactor helper functions.
    In [template]/page.js, update imports and refactor getTemplate function.
    
    Changes include:
    - removing unused variables
    - updating imports
    - refactoring helper functions
    - refactoring getTemplate function to handle errors and revalidate cache.

--- src/app/dashboard/settings/[setting]/page.jsx
+++ src/app/dashboard/settings/[setting]/page.jsx
@@ -4,81 +4,80 @@ import { Suspense } from 'react';
 import SettingsForm from '@/app/_components/forms/settingsForm/SettingsForm';
 
 export async function generateStaticParams() {
-  const settings = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/settings`
-  ).then((res) => res.json());
+	const settings = await fetch(
+		`${process.env.NEXT_PUBLIC_BASE_URL}/api/settings`
+	).then((res) => res.json());
 
-  console.log(settings);
+	let payload = settings.reduce((acc, currentValue) => {
+		Object.entries(currentValue.groupName).map((item) => {
+			acc.push({
+				setting: item[1].toLowerCase().split(' ').join('-'),
+			});
+		});
+		return acc;
+	}, []);
 
-  return settings.map((setting) => {
-    return Object.entries(setting.groupName).map((item) => ({
-      setting: item[1],
-    }));
-    // console.log(...payload, 'THE PAYLOAD');
-    // return { ...payload };
-  });
+	return payload;
 }
 
 export async function getSettingGroup(setting, lang) {
-  try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/settings/${setting}?lang=${lang}`,
-      {
-        next: { tags: ['setting'], revalidate: 3600 },
-      }
-    );
-    if (!res.ok) {
-      throw new Error('Faliled to fetch.statusText data: ' + res.data);
-    }
-    return await res.json();
-  } catch (error) {
-    console.log(error);
-    return new Error('Error: ' + error);
-  }
+	try {
+		const res = await fetch(
+			`${process.env.NEXT_PUBLIC_BASE_URL}/api/settings/${setting}?lang=${lang}`,
+			{
+				next: { tags: ['setting'], revalidate: 3600 },
+			}
+		);
+		if (!res.ok) {
+			throw new Error('Faliled to fetch.statusText data: ' + res.data);
+		}
+		return await res.json();
+	} catch (error) {
+		console.log(error);
+		return new Error('Error: ' + error);
+	}
 }
 
 // << helper functions
 function setDataForFrontEnd(data) {
-  let collection = data?.collection?.reduce((acc, currentValue) => {
-    let value = {
-      id: currentValue._id,
-      item: currentValue[data.collectionType],
-    };
-    acc.push(value);
-    return acc;
-  }, []);
+	let collection = data?.collection?.reduce((acc, currentValue) => {
+		let value = {
+			id: currentValue._id,
+			item: currentValue[data.collectionType],
+		};
+		acc.push(value);
+		return acc;
+	}, []);
 
-  const payload = {
-    groupName: data.groupName,
-    collectionType: data.collectionType,
-    collection: { [data.collectionType]: collection },
-  };
+	const payload = {
+		groupName: data.groupName,
+		collectionType: data.collectionType,
+		collection: { [data.collectionType]: collection },
+	};
 
-  return payload;
+	return payload;
 }
 // helper functions >>
 
 export const dynamic = 'force-dynamic';
 export const dynamicParams = true;
 
-// Работи само со ова(приметив каснење, да видам дали ќе се случи пак), но ќе остаам да видам уште некој ден. 11.03.2024
 export const revalidate = 0;
 
 export default async function Edit({ params, searchParams }) {
-  // console.log(params, 'the params');
-  const settingForDb = params.setting.toLowerCase().split('-').join(' ');
+	console.log(params, 'the params');
+	const settingForDb = params.setting.toLowerCase().split('-').join(' ');
 
-  const lang = searchParams.lang || 'en'; // Место англиски, треба да биде избран стандарден јазик од база
-  // const lang = 'en';
+	const lang = searchParams.lang || 'en'; // Место англиски, треба да биде избран стандарден јазик од база
 
-  const data = setDataForFrontEnd(await getSettingGroup(settingForDb, lang));
+	const data = setDataForFrontEnd(await getSettingGroup(settingForDb, lang));
 
-  return (
-    <div className='w-1/2 px-2'>
-      {/* <h2>Edit Setting</h2> */}
-      <Suspense fallback={<span>Loading...</span>}>
-        <SettingsForm data={data} shouldUpdate={true} />
-      </Suspense>
-    </div>
-  );
+	return (
+		<div className='w-1/2 px-2'>
+			{/* <h2>Edit Setting</h2> */}
+			<Suspense fallback={<span>Loading...</span>}>
+				<SettingsForm data={data} shouldUpdate={true} />
+			</Suspense>
+		</div>
+	);
 }
--- src/app/dashboard/templates/[template]/page.js
+++ src/app/dashboard/templates/[template]/page.js
@@ -3,40 +3,43 @@ import { Suspense } from 'react';
 // components
 import TemplateForm from '@/app/_components/forms/templateForm/TemplateForm';
 import { getTemplateData } from '../create/page';
-import { getTemplate } from '@/app/_actions/temlatesActions';
 
 export async function generateStaticParams() {
 	const templates = await fetch(
 		`${process.env.NEXT_PUBLIC_BASE_URL}/api/templates`
 	).then((res) => res.json());
 
-	return templates.map((template) => ({
-		template: template._id.toString(),
-	}));
+	return templates.map((template) => {
+		return {
+			template: template._id.toString(),
+		};
+	});
 }
 
-// export async function getTemplate(template) {
-// 	const res = await fetch(
-// 		`${process.env.NEXT_PUBLIC_BASE_URL}/api/templates/${template}`,
-// 		{
-// 			next: { tags: ['template'], revalidate: 3600, cache: 'no-store' },
-// 		}
-// 	);
+export async function getTemplate(template) {
+	const res = await fetch(
+		`${process.env.NEXT_PUBLIC_BASE_URL}/api/templates/${template}`,
+		{
+			next: { tags: ['template'], revalidate: 3600 },
+			// cache: 'no-store'
+		}
+	);
 
-// 	if (!res.ok) {
-// 		throw new Error('Failed to fetch data: ' + res);
-// 	}
+	if (!res.ok) {
+		throw new Error('Failed to fetch data: ' + res);
+	}
 
-// 	return await res.json();
-// }
+	// од тука тргнав await
+	return res.json();
+}
+
 export const dynamic = 'force-dynamic';
 // export const dynamicParams = true;
 
 export const revalidate = 0;
 export default async function EditTemplate({ params }) {
 	const { template } = params;
-	// console.log(params, 'the params');
-	const templateData = JSON.parse(await getTemplate(template));
+	const templateData = await getTemplate(template);
 	const getTemplateSettings = await getTemplateData(template);
 
 	return (
